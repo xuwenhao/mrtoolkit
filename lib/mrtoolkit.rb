e37d6159f8bca0da22c6f3ede1cbf78471ec0214
Xu Wenhao <wenhao.xu@gmail.com>
2011-03-22 17:54:54 +0800

    fix the typo, it should be case...then, not 'when'

--- lib/mrtoolkit.rb
+++ lib/mrtoolkit.rb
@@ -775,10 +775,10 @@ class JobBase
   # This gathers the declarations and stores in a stage record.
   def add_stage
     case
-    when @map_class.nil? when raise "Map class not specified"
-    when @reduce_class.nil? when raise "Reduce class not specified"
-    when @in_dirs.empty? when raise "Indir not speficied"
-    when @out_dir.nil? when raise "Outdir not specified"
+    when @map_class.nil? then raise "Map class not specified"
+    when @reduce_class.nil? then raise "Reduce class not specified"
+    when @in_dirs.empty? then raise "Indir not speficied"
+    when @out_dir.nil? then raise "Outdir not specified"
     end       
     @stages << [@map_class, @map_args, @map_opts,
       @reduce_class, @reduce_args, @reduce_opts, 
